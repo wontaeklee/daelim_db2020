d13d6d675395b1cfa57f38054ec8ca7a346ba4f3
wontaeklee <tovie200@gmail.com>
2020-04-29 21:39:54 +0900

    2020-04-29

---
+++ modules/wontaek/hello.php
@@ -0,0 +1,13 @@
+<?php
+namespace Modules\Wontaek;
+
+// 클래스 선언
+class Hello
+{
+    // 클래스의 함수
+    // 메소드 함.
+    public function greeting()
+    {
+        echo "Wontaek!! hello world";
+    }
+}
